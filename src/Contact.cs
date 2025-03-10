912e368bd5e904b3eace5a63d187d2b7faf67e15
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-03-10 16:34:03 -0700

    Added Contact fields, both Contact constructors, and the beginning of override CompareTo method

--- src/Contact.cs
+++ src/Contact.cs
@@ -1,6 +1,45 @@
 ﻿using System;
 
+using System;
+
 public class Contact : IComparable<Contact>
 {
+    private string fName;
+    private string lName;
+    private string phoneNum;
+    private string address;
+    private int zipCode;
+    private string email;
 
+    public Contact(string fName, string lName, string phoneNum, string address, int zipCode, string email)
+    {
+        this.fName = fName;
+        this.lName = lName;
+        this.phoneNum = phoneNum;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.email = email;
+    }
+
+    public Contact(string fName, string lName, string phoneNum)
+    {
+        this.fName = fName;
+        this.lName = lName;
+        this.phoneNum = phoneNum;
+    }
+
+
+
+    // override
+    public int CompareTo(Contact obj)
+    {
+        string chars = "abcdefghijklmnopqrstuvwxyz";
+        bool fNameEnd = false;
+        bool inputFNameEnd = false;
+        int charCounter = 1;
+        int fNameIndex;
+        int inputFNameIndex;
+
+        return 0;
+    }
 }
