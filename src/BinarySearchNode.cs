2af7bb6c41fad14f4f45562fe21ca8b954019dc5
Christopher Waschke <chrisdwaschke@gmail.com>
2025-03-10 23:10:56 -0700

    Finish porting to BinarySearchNode to C#.

--- src/BinarySearchNode.cs
+++ src/BinarySearchNode.cs
@@ -2,11 +2,12 @@
 
 namespace src;
 
-public class BinarySearchNode<T> : IComparable where T : IComparable 
+
+public class BinarySearchNode<T> where T : IComparable  // Translation: BinarySearchNode, with generic type T that implements IComparable.
 {
     private T value;
-    private BinarySearchNode<T> left;
-    private BinarySearchNode<T> right;
+    private BinarySearchNode<T>? left; // Note: C# is a slightly more modern language than Java, as such ? following a type means that it could be null.
+    private BinarySearchNode<T>? right;
 
     public BinarySearchNode(T value)
     {
@@ -21,12 +22,11 @@ public class BinarySearchNode<T> : IComparable where T : IComparable
     public BinarySearchNode<T> Left
     {
         set { this.left = value; }
-        get { return this.left; }
+        get { return this.left; } // This is okay to be null, as null means that we're a leaf, or at least a one way branch.
     }
     public BinarySearchNode<T> Right
     {
         set { this.right = value; }
-        get { return this.right; }
+        get { return this.right; } // This is okay to be null, as null means that we're a leaf, or at least a one way branch.
     }
-
 }
