aefb4dc1c24e3e40d2fdc5c1636bb521ed6eca4b
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-03-10 17:17:34 -0700

    Translated BinarySearchNode class

---
+++ src/BinarySearchNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class BinarySearchNode<T> : IComparable where T : IComparable 
+{
+    private T value;
+    private BinarySearchNode<T> left;
+    private BinarySearchNode<T> right;
+
+    public BinarySearchNode(T value)
+    {
+        this.value = value;
+    }
+
+    public T Value
+    {
+        set { this.value = value; }
+    }
+
+    public BinarySearchNode<T> Left
+    {
+        set { this.left = value; }
+        get { return this.left; }
+    }
+    public BinarySearchNode<T> Right
+    {
+        set { this.right = value; }
+        get { return this.right; }
+    }
+
+}
