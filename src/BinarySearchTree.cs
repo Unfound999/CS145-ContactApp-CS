497f74bb0265a2b8ccf7d0fd2fdc652b8310f823
Christopher Waschke <Chrisdwaschke@gmail.com>
2025-03-11 22:28:47 -0700

    Ported constructors, and add method.

--- src/BinarySearchTree.cs
+++ src/BinarySearchTree.cs
@@ -13,5 +13,50 @@ public class BinarySearchTree<T> where T : IComparable
         POSTORDER
     }
 
-    private BinaryTreeNode<T> root;
+    private BinaryTreeNode<T>? root;
+
+    public BinarySearchTree()
+    {
+        this.root = null;
+    }
+
+    public BinarySearchTree(T value)
+    {
+        this.root = new BinaryTreeNode<T>(value);
+    }
+
+    public void add(T newData)
+    {
+        if (root == null)
+        {
+            this.root = new BinaryTreeNode<T>(newData);
+            return;
+        }
+        BinaryTreeNode<T> currNode = root;
+        while (currNode != null)
+        {
+            if (currNode.Value.Equals(newData))
+            {
+                throw new Exception();
+            }
+            if (currNode.Value.CompareTo(newData) > 0)
+            {
+                if (currNode.Left == null)
+                {
+                    currNode.Left = new BinaryTreeNode<T>(newData);
+                    break;
+                }
+                currNode = currNode.Left;
+            }
+            if((currNode.Value.CompareTo(newData) < 0))
+            {
+                if (currNode.Right == null)
+                {
+                    currNode.Right = new BinaryTreeNode<T>(newData);
+                    break;
+                }
+                currNode = currNode.Right;
+            }
+        }
+    }
 }
