2572a33b921972eded25990d5c973580ebd29761
Christopher Waschke <Chrisdwaschke@gmail.com>
2025-03-11 22:17:26 -0700

    Rename BinarySearchNode to BinaryTreeNode to match Java version.

--- src/BinaryTreeNode.cs
+++ src/BinaryTreeNode.cs
@@ -3,13 +3,13 @@
 namespace src;
 
 
-public class BinarySearchNode<T> where T : IComparable  // Translation: BinarySearchNode, with generic type T that implements IComparable.
+public class BinaryTreeNode<T> where T : IComparable  // Translation: BinarySearchNode, with generic type T that implements IComparable.
 {
     private T value;
-    private BinarySearchNode<T>? left; // Note: C# is a slightly more modern language than Java, as such ? following a type means that it could be null.
-    private BinarySearchNode<T>? right;
+    private BinaryTreeNode<T>? left; // Note: C# is a slightly more modern language than Java, as such ? following a type means that it could be null.
+    private BinaryTreeNode<T>? right;
 
-    public BinarySearchNode(T value)
+    public BinaryTreeNode(T value)
     {
         this.value = value;
     }
@@ -19,12 +19,12 @@ public class BinarySearchNode<T> where T : IComparable  // Translation: BinarySe
         set { this.value = value; }
     }
 
-    public BinarySearchNode<T> Left
+    public BinaryTreeNode<T> Left
     {
         set { this.left = value; }
         get { return this.left; } // This is okay to be null, as null means that we're a leaf, or at least a one way branch.
     }
-    public BinarySearchNode<T> Right
+    public BinaryTreeNode<T> Right
     {
         set { this.right = value; }
         get { return this.right; } // This is okay to be null, as null means that we're a leaf, or at least a one way branch.
