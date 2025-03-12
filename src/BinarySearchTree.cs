3c5a3ef09cabf2cd3d43863131db4d1b018da977
Christopher Waschke <Chrisdwaschke@gmail.com>
2025-03-11 22:18:00 -0700

    Fixed generic constraints syntax.

--- src/BinarySearchTree.cs
+++ src/BinarySearchTree.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace src;
 
 //  Unsure how to translate implement IComparable class. It currently does not work (Check errors on Error List)
-public class BinarySearchTree<T> : IComparable where T : IComparable 
+public class BinarySearchTree<T> where T : IComparable 
 {
     enum SearchType
     {
